9e651cf2cb58809315850aaffc0b57adebffc6fa
jonathan <jonathancastro2018@yahoo.com>
2020-03-07 21:31:38 +0100

    agregando el formulario con bootstrap 4

--- application/views/login.php
+++ application/views/login.php
@@ -1,9 +1,42 @@
 <div class="row">
 
 	<div class="container">
-		
-		<h1 class="text-danger">login</h1>
+
+		<!-- <h1 class="text-danger">login</h1> -->
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+
+
+		<div class="col-4 offset-4">
+
+			<h4>Iniciar session</h4>
+
+
+			<?php echo form_open('login/signin') ?>
+
+
+			<input type="text" class="form-control" name="email" placeholder="email">
+			<br>
+
+			<input type="text" class="form-control" name="password" placeholder="password">
+			<br>
+
+
+			<button type="submit" class="btn btn-info">Enviar</button>
+
+
+			<?php echo form_close()  ?>
+
+		</div>
 
 	</div>
 
 </div>
+<br>
+<br>
+<br>
+<br>
+<br>
